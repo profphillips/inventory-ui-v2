c16c46a2fea5e7a14f0f98e99cb1a4aab899b910
JP <noreply>
2021-04-05 12:12:56 -0400

    print buy list with print media css

--- src/print.js
+++ src/print.js
@@ -1,11 +1,48 @@
+import React, { useState, useEffect } from "react";
+import { dbGetAllData } from "./dbapi";
 import Footer from "./footer";
+
 export default function Print() {
+  let [dataArray, setDataArray] = useState([]);
+
+  useEffect(() => {
+    dbGetAllData(setDataArray);
+  }, []); // ignore warning - empty dependency array [] so only executed once
+
+  useEffect(() => {
+    console.log("second useEffect runs whenever the dataArray changes");
+    console.log(dataArray);
+  }, [dataArray]);
+
+  const buyData = dataArray.filter((row) => row.buy === true);
+  console.log("bd=", buyData);
+
   return (
     <>
       <section>
-        <h3 id="print">Print</h3>
+        <h3>Buy List</h3>
+        <table>
+          <thead>
+            <tr>
+              <th>Qty</th>
+              <th>Item</th>
+            </tr>
+          </thead>
+          <tbody>
+            {buyData.map((oneRow) => {
+              return (
+                <>
+                  <tr key={oneRow._id}>
+                    <td>{oneRow.qty}</td>
+                    <td>{oneRow.name}</td>
+                  </tr>
+                </>
+              );
+            })}
+          </tbody>
+        </table>
+        <Footer />
       </section>
-      <Footer />
     </>
   );
 }
